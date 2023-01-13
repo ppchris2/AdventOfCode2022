3179a77bff484043ea192399390243194b95f0b7
Christos <ppchris2@gmail.com>
2023-01-14 00:18:21 +0200

    Day1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,52 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
+use std::fs;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
 
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    // let string = "1000
+    // 2000
+    // 3000
+    
+    // 4000
+    
+    // 5000
+    // 6000
+    
+    // 7000
+    // 8000
+    // 9000
+    
+    // 10000";
+
+    let contents = fs::read_to_string("./artifacts/Day01/input.txt")
+        .expect("Should have been able to read the file");
+
+
+    let lines = contents.lines();
+    let mut vec = Vec::new();
+    let mut count = 0;
+
+    for item in lines {
+
+        
+        let trimmed = item.trim().to_string();
+        if trimmed == ""  {
+            vec.push(count);
+            count = 0;
+        }
+        
+        else{
+            let num : u32 = trimmed.parse().expect("What");
+            count = num + count
+        }
+    }
+    vec.push(count);
+    vec.sort();
+
+    let answer1 = vec.last().unwrap();
+    println!("answer1: {}", answer1);
+    let sum2 = &vec[vec.len()-3..];
+    
+    let sum: u32 = sum2.iter().sum();
+
+    println!("answer2: {}", sum)
 }
